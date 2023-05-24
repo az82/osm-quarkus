45596b27da54e6e7502f66545bf3904a136cf773
Andreas Zitzelsberger <az@az82.de>
2023-05-24 12:25:43 +0200

    Security augmentor for non-OIDC compliant providers added

---
+++ src/main/java/de/az82/demo/osmquarkus/OAuth2IdentityAugmentor.java
@@ -0,0 +1,48 @@
+package de.az82.demo.osmquarkus;
+
+import io.quarkus.logging.Log;
+import io.quarkus.oidc.runtime.OidcJwtCallerPrincipal;
+import io.quarkus.security.identity.AuthenticationRequestContext;
+import io.quarkus.security.identity.SecurityIdentity;
+import io.quarkus.security.identity.SecurityIdentityAugmentor;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.jose4j.jwt.MalformedClaimException;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Identity augmentor for non-OIDC compliant OAuth2 providers.
+ *
+ * If Quarkus uses a non-OIDC compliant OAuth2 provider that does not provide an Id token, Quarkus will create a
+ * "virtual" Id token. In this case, Quarkus calls the userinfo endpoint of the provider, but does not populate the Id
+ * token with user information.
+ *
+ * This augmentor populates the Id token with information from the userinfo endpoint.
+ */
+@ApplicationScoped
+public class OAuth2IdentityAugmentor implements SecurityIdentityAugmentor {
+
+    @Override
+    public Uni<SecurityIdentity> augment(SecurityIdentity identity, AuthenticationRequestContext context) {
+        try {
+            var principal = identity.getPrincipal();
+            if (principal instanceof OidcJwtCallerPrincipal jwtPrincipal) {
+                var claims = jwtPrincipal.getClaims();
+
+                var userinfo = claims.getClaimValue("userinfo", Map.class);
+                if (claims.getSubject() == null && userinfo != null) {
+                    // Use the login field.
+                    // Tested with GitHub
+                    claims.setSubject(Objects.toString(userinfo.get("login"), null));
+                }
+            }
+        } catch (MalformedClaimException e) {
+            Log.error("Malformed token", e);
+        }
+
+        return Uni.createFrom().item(identity);
+    }
+
+}
--- src/main/java/de/az82/demo/osmquarkus/OpaHttpSecurityPolicy.java
+++ src/main/java/de/az82/demo/osmquarkus/OpaHttpSecurityPolicy.java
@@ -1,7 +1,6 @@
 package de.az82.demo.osmquarkus;
 
 import io.quarkus.logging.Log;
-import io.quarkus.oidc.runtime.OidcJwtCallerPrincipal;
 import io.quarkus.security.identity.SecurityIdentity;
 import io.quarkus.vertx.http.runtime.security.HttpSecurityPolicy;
 import io.smallrye.mutiny.Uni;
@@ -39,7 +38,7 @@ public class OpaHttpSecurityPolicy implements HttpSecurityPolicy {
                     var method = request.method().name();
                     var path = request.path();
                     var headers = flattenHeaders(request.headers());
-                    var principal = getPrincipalName(identity);
+                    var principal = identity.getPrincipal().getName();
 
                     return Uni.createFrom().completionStage(
                             opaRestClient.allow(new OpaRequest(new OpaRequest.Input(
@@ -67,25 +66,6 @@ public class OpaHttpSecurityPolicy implements HttpSecurityPolicy {
                 }
 
         );
-    }
-
-    private static String getPrincipalName(SecurityIdentity identity) {
-        // Unauthenticated -> No principal name
-        if (identity.isAnonymous()) {
-            return null;
-        }
-
-        var principalName = identity.getPrincipal().getName();
-        if (principalName != null) {
-            // Default: Quarkus has correctly identified the principal
-            return principalName;
-        } else {
-            // GitHub is not OIDC compliant and does not provide an Id token
-            // Quarkus creates a "virtual" token but does not map the login userinfo field to the virtual token's
-            // sub claim. So we get the GitHub login from userinfo and treat that as principal name.
-            var principal = (OidcJwtCallerPrincipal) identity.getPrincipal();
-            return ((Map<?, ?>) principal.getClaim("userinfo")).get("login").toString();
-        }
     }
 
     private static Map<String, String> flattenHeaders(MultiMap headers) {
